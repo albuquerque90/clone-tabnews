3b1e224203d658774bbc507a4784f18a6c4c3dff
Fernando Albuquerque <fer.albuquerque90@gmail.com>
2025-12-30 19:21:14 -0300

    modifica frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Juliana, meu amor! O que a vida quer da gente é coragem e você me deixa muito mais corajoso!</h1>
+    return <h1>Juliana, meu amor! O que a vida quer da gente é coragem e você me deixa muito mais corajoso! 💛</h1>
 }
 
 export default Home;
