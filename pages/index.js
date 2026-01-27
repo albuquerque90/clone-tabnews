f804a21319de26a7d27ec766d0aa670885a62c17
Fernando Albuquerque <fer.albuquerque90@gmail.com>
2026-01-27 18:46:13 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Juliana, meu amor! O que a vida quer da gente é coragem e você me deixa muito mais corajoso! 💛</h1>
+  return (
+    <h1>
+      Juliana, meu amor! O que a vida quer da gente é coragem e você me deixa
+      muito mais corajoso! 💛
+    </h1>
+  );
 }
 
 export default Home;
