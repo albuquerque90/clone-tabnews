6e130349077d5245799a151399068960fbe6d9e0
Fernando Albuquerque <fer.albuquerque90@gmail.com>
2026-02-19 11:07:58 -0300

    registrando objetivo do projeto

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ function Home() {
   return (
     <h1>
       Juliana, meu amor! O que a vida quer da gente é coragem e você me deixa
-      muito mais corajoso! 💛
+      muito mais corajoso! 💛 este projeto é para praticar e reciclar meus
+      conhecimentos em programação!
     </h1>
   );
 }
